d7ae85beb15c91b1de07949622bc9e16c4bff792
CoodeNoob <swanhtetpyaesone323@gmail.com>
2025-09-17 12:00:36 +0630

    added_header&footer

---
+++ LAYOUTS/layoutLoading.js
@@ -0,0 +1,12 @@
+// LOADING THE FOOTER AND HEADER 
+fetch('../LAYOUTS/footer.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('footer_block').innerHTML = data;
+    });
+
+fetch('../LAYOUTS/header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header_block').innerHTML = data;
+    });
