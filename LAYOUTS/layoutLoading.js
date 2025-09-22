89e01d1ccfa06eadf3494c1cf6db7681ad0b2bf2
CoodeNoob <swanhtetpyaesone323@gmail.com>
2025-09-22 16:49:34 +0630

    fix_navbar_footer_

--- LAYOUTS/layoutLoading.js
+++ LAYOUTS/layoutLoading.js
@@ -1,17 +1,17 @@
 // LOADING THE FOOTER AND HEADER 
-fetch('../LAYOUTS/footer.html')
+fetch('./LAYOUTS/footer.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('footer_block').innerHTML = data;
     });
 
-fetch('../LAYOUTS/header.html')
+fetch('./LAYOUTS/header.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('header_block').innerHTML = data;
     });
 
-fetch('../LAYOUTS/loader.html')
+fetch('./LAYOUTS/loader.html')
     .then(response => response.text())
     .then(data => {
         document.getElementById('loader-container').innerHTML = data;
